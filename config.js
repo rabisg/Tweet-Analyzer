516e77539e2aa195fde107e576d49d4d6cf375a0
rabisg <guha.rabishankar@gmail.com>
2012-08-26 15:23:44 +0530

    :p

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config = {};
 
 config.port = 80;
 config.address = 'localhost';
-config.twitter_username = process.env.TWITTER_USERNAME || 'shikhar_tweet';
-config.twitter_password = process.env.TWITTER_PASSWORD || 'abcd1234abcd';
+config.twitter_username = process.env.TWITTER_USERNAME || 'your_username';
+config.twitter_password = process.env.TWITTER_PASSWORD || 'your_password';
 config.consumer_key = '36zzzdzo4eX58vxW2znoTw';
 config.consumer_secret = 'N9guGb0xQg79HonQZzNEPBS9hlm57DCm5oJaPOCLse0';
 
